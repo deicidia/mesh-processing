ec9445e5838aaf02b2882137364c70115cdffd8a
eiSS <thoum.nathan@gmail.com>
2026-09-22 02:37:01 +0200

    refactor: rename HelloKokkos test target and source file to kokkos_info

--- test/kokkos_info.cpp
+++ test/kokkos_info.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-
+  
   Kokkos::initialize(argc, argv);
 
   Kokkos::print_configuration(std::cout);
 
   Kokkos::finalize();
-  
+
   return 0;
 }
